a8d1a2dbf992edf01f9c8bc259daa4bde6509676
safwansatil <asf1k.til@gmail.com>
2025-04-26 01:08:30 +0600

    A basic Chatbot made

---
+++ app.js
@@ -0,0 +1,13 @@
+// import { GoogleGenAI } from "@google/genai";
+
+// const ai = new GoogleGenAI({ apiKey: "AIzaSyBs381LGOePP1TSoZOEDAGAiLm11ypsInU" });
+
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.0-flash",
+//     contents: "How does AI work?",
+//   });
+//   console.log(response.text);
+// }
+
+// await main();
